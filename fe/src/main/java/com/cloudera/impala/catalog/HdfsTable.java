055408f84a61f667a376e27b3bf02a56e93acc3b
CC Lien <cclien@gmail.com>
2014-02-14 15:26:07 +0800

    Fix IMPALA-749. Allow to load partitions more than Short.MAX_VALUE. (backport for v1.2.2, v1.2.3)

--- fe/src/main/java/com/cloudera/impala/catalog/HdfsTable.java
+++ fe/src/main/java/com/cloudera/impala/catalog/HdfsTable.java
@@ -135,6 +135,9 @@ public class HdfsTable extends Table {
 
   private static final boolean SUPPORTS_VOLUME_ID;
 
+  // Indicating no partition limit in listPartition API of HiveMetaStoreClient
+  private static final short UNLIMITED_NUM_PARTITIONS=-1;
+
   static {
     try {
       // call newInstance() instead of using a shared instance from a cache
@@ -695,7 +698,7 @@ public class HdfsTable extends Table {
           Lists.newArrayList();
       if (cachedEntry == null || !(cachedEntry instanceof HdfsTable) ||
           cachedEntry.lastDdlTime != lastDdlTime) {
-        msPartitions.addAll(client.listPartitions(db.getName(), name, Short.MAX_VALUE));
+        msPartitions.addAll(client.listPartitions(db.getName(), name, UNLIMITED_NUM_PARTITIONS));
       } else {
         // The table was already in the metadata cache and it has not been modified.
         Preconditions.checkArgument(cachedEntry instanceof HdfsTable);
@@ -712,7 +715,7 @@ public class HdfsTable extends Table {
           // First get a list of all the partition names for this table from the
           // metastore, this is much faster than listing all the Partition objects.
           modifiedPartitionNames.addAll(
-              client.listPartitionNames(db.getName(), name, Short.MAX_VALUE));
+              client.listPartitionNames(db.getName(), name, UNLIMITED_NUM_PARTITIONS));
         }
 
         int totalPartitions = modifiedPartitionNames.size();
